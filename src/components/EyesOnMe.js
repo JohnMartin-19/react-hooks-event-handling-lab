e23fc23b12d7182e3f3c1b888640e42c2e77e1ee
John <johnnkonge2020@gmail.com>
2023-10-27 09:46:11 +0300

    done!

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    return(
+        <button onFocus={
+            console.log('Good')
+        } onBlur={
+            console.log('Hey! Eyes on me mf!!')
+        }>EyesOnMe</button>
+    )
+}
+
+export default EyesOnMe
