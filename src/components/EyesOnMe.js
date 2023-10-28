45a3666349babfef349b48ab85481bfcef3fdd50
John <johnnkonge2020@gmail.com>
2023-10-28 15:36:36 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,9 +1,9 @@
 // Code EyesOnMe Component Here
 function EyesOnMe(){
     return(
-        <button onFocus={
+        <button onFocus={event =>
             console.log('Good')
-        } onBlur={
+        } onBlur={event =>
             console.log('Hey! Eyes on me mf!!')
         }>EyesOnMe</button>
     )
